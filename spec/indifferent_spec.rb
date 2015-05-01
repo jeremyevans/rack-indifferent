6ad5ca5aa0fbcc3ffa37cf8a9c67b4b5870c8aa0
Jeremy Evans <code@jeremyevans.net>
2015-05-01 14:12:08 -0700

    Switch from rspec to minitest/spec

--- spec/indifferent_spec.rb
+++ spec/indifferent_spec.rb
@@ -1,25 +1,15 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), '../lib/rack/indifferent')
 require 'rack/mock'
-
-if defined?(RSpec)
-  require 'rspec/version'
-  if RSpec::Version::STRING >= '2.11.0'
-    RSpec.configure do |config|
-      config.expect_with :rspec do |c|
-        c.syntax = :should
-      end
-    end
-  end
-end
+require 'minitest/autorun'
 
 describe 'rack-indifferent' do
   it "should make requests use indifferent param hashes" do
     params = Rack::Request.new(Rack::MockRequest.env_for('/?a=b&c[d]=e&f[][g]=h', :input=>'i=j&k[l]=m&n[][o]=p', :method=>'POST')).params
-    params[:a].should == 'b'
-    params[:c][:d].should == 'e'
-    params[:f][0][:g].should == 'h'
-    params[:i].should == 'j'
-    params[:k][:l].should == 'm'
-    params[:n][0][:o].should == 'p'
+    params[:a].must_equal 'b'
+    params[:c][:d].must_equal 'e'
+    params[:f][0][:g].must_equal 'h'
+    params[:i].must_equal 'j'
+    params[:k][:l].must_equal 'm'
+    params[:n][0][:o].must_equal 'p'
   end
 end
