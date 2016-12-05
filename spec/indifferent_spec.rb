033d3fb80871e91de1bdb8869bec28c3db51e965
Jeremy Evans <code@jeremyevans.net>
2016-12-05 11:28:52 -0800

    Change must_equal nil to must_be_nil in specs
    Minitest 5.10 started warning about must_equal nil.

--- spec/indifferent_spec.rb
+++ spec/indifferent_spec.rb
@@ -15,7 +15,7 @@ describe 'rack-indifferent' do
 
   it "should make requests handle empty params" do
     params = Rack::Request.new(Rack::MockRequest.env_for('/', :input=>'i=j&k[l]=m&n[][o]=p', :method=>'POST')).params
-    params[:a].must_equal nil
+    params[:a].must_be_nil
     params[:i].must_equal 'j'
     params[:k][:l].must_equal 'm'
     params[:n][0][:o].must_equal 'p'
